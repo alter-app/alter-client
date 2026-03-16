2403a4a63f8de66f63d095d98a7fc2fa25e7e9a4
Dohyeon <do.hy824@gmail.com>
2026-03-16 15:46:28 +0900

    fix: 빌드 에러 수정

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { ManagerHomePage } from '@/pages/manager/home'
 import { LoginPage } from '@/pages/login'
 import { SignupPage } from '@/pages/signup'
-import { JobLookupMapPage } from '@/pages/job-lookup-map'
+import { JobLookupMapPage } from '@/pages/user/job-lookup-map'
 import { SchedulePage } from '@/pages/user/schedule'
 
 export function App() {
