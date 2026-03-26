f7d45be45e23d96a947b18c8d8f0624fafefb064
limtjdghks <epaktldk0203@gmail.com>
2026-03-26 15:36:11 +0900

    chore: svgr 플러그인 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 import path from 'path'
 import { fileURLToPath } from 'node:url'
 
@@ -7,7 +8,7 @@ const dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       '@': path.resolve(dirname, './src'),
