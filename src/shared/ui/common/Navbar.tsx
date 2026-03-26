e8b1d1064d720e455a69a3e5ea27bc9b8d2f7e1e
kimtaewoo <70637743+kim3360@users.noreply.github.com>
2026-03-27 00:22:30 +0900

    Revert "feat : Navbar 아이콘 추가"
    This reverts commit 86f4f3f08a2ab231cc3ea261ec9412447d6a68ea.

--- src/shared/ui/common/Navbar.tsx
+++ src/shared/ui/common/Navbar.tsx
@@ -1,18 +1,10 @@
 import AlterLogo from '@/assets/Alter-logo.png'
-import AlterLogotext from '@/assets/Alter-logo-text.png'
-import BellIcon from '@/assets/icons/bell.svg'
-import MenuIcon from '@/assets/icons/menu.svg'
 
 export function Navbar() {
   return (
-    <div className="flex items-center justify-between w-full border-b border-line-1 px-4 h-14 bg-white">
-      <div className="flex items-center gap-2">
-        <img src={AlterLogo} alt="logo" className="w-7 h-7" />
-        <img src={AlterLogotext} alt="logo-text" />
-      </div>
-      <div className="flex items-center gap-4 w-6 h-6">
-        <img src={BellIcon} alt="bell" className="cursor-pointer" />
-        <img src={MenuIcon} alt="menu" className="cursor-pointer" />
+    <div className="flex items-center justify-between w-full border border-line-1 px-4 h-14 bg-white">
+      <div className="flex items-center w-6 h-6">
+        <img src={AlterLogo} alt="logo" />
       </div>
     </div>
   )
