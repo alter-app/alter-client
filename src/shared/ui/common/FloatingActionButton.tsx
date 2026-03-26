4c94bf1fbc59afc44935d3bd44e6437ef4145c05
kimtaewoo <70637743+kim3360@users.noreply.github.com>
2026-03-27 02:58:38 +0900

    feat : FloatingActionButton 추가

---
+++ src/shared/ui/common/FloatingActionButton.tsx
@@ -0,0 +1,12 @@
+import PlusIcon from '@/assets/icons/plus.svg'
+export function FloatingActionButton() {
+  return (
+    <button
+      type="button"
+      className="h-14 w-14 shrink-0 rounded-full bg-main text-4xl text-white leading-none cursor-pointer inline-flex items-center justify-center aspect-square"
+      aria-label="추가"
+    >
+      <img src={PlusIcon} alt="plus" />
+    </button>
+  )
+}
